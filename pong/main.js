1a8179320da74d6dc246b04512a6633b64502bc8
duvanleal96 <duvanleal65@gmail.com>
2022-06-04 22:30:24 -0500

    implementacion barras laterales
    se crean las 2 barras con las que va a interactuar la pelota de ping pong

--- pong/main.js
+++ pong/main.js
@@ -9,13 +9,92 @@
     }
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars.map(function (bar) { return bar; });
-            elements.push(this.ball);
+            var elements = this.bars;
+           // elements.push(this.ball);
             return elements;
         }
     }
 })();
+(function () {
+    self.Ball = function (x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+      console.log("hhhh");
+      
+
+    }
+
+})();
+(function () {
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    
+
+    }
+    self.Bar.prototype = {
+        down: function () {
+            
+        },
+        up: function () {
+           
+        },
+       
+    }
+})();
+(function () {
+    self.BoardView = function (canvas, board) {
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+    self.BoardView.prototype = {
+        clean: function () {
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.ctx, el);
+            }
+        },
+        
+        play: function () {
+
+                this.draw();
+
+        }
+
+    }
+
+    function draw(ctx, element) {
+        switch (element.kind) {
+            case "rectangle":
+                ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+          
+        }
+    }
+
+
+})();
+
 window.requestAnimationFrame(main);
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board)
+var bar2 = new Bar(740, 100, 40, 100, board)
 
 function main() {
     window.requestAnimationFrame(main);
