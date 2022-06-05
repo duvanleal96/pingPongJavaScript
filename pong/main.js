64d5231b24001703e03bedcf1f35f5a33e00a85a
duvanleal96 <duvanleal65@gmail.com>
2022-06-04 23:36:18 -0500

    implementacion movimiento grafico de los rectangulos
    se implementa el movimiento por pantalla de los rectangulos

--- pong/main.js
+++ pong/main.js
@@ -9,8 +9,8 @@
     }
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars;
-           // elements.push(this.ball);
+            var elements = this.bars.map(function (bar) { return bar; });
+            elements.push(this.ball);
             return elements;
         }
     }
@@ -23,12 +23,12 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
     
-        board.ball = this;
-        this.kind = "circle";
+     
 
     }
-  
+   
 })();
 (function () {
     self.Bar = function (x, y, width, height, board) {
@@ -73,37 +73,31 @@
                 draw(this.ctx, el);
             }
         },
-       
+        
         play: function () {
-            
+            if (this.board.playing) {
                 this.clean();
                 this.draw();
-        
+               
+            }
 
         }
 
     }
-
+  
     function draw(ctx, element) {
         switch (element.kind) {
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
                 break;
-        
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
-    document.addEventListener("keydown", function (ev) {
-
-        if (ev.keyCode == 38) {
-            ev.preventDefault();
-            bar.up();
-        }
-        else if (ev.keyCode == 40) {
-            ev.preventDefault();
-            bar.down();
-        }
-        
-    });
 
 
 })();
@@ -111,7 +105,30 @@ var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board)
 var bar2 = new Bar(740, 100, 40, 100, board)
 var canvas = document.getElementById("canvas");
+var boardview = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
 
+
+document.addEventListener("keydown", function (ev) {
+
+    if (ev.keyCode == 38) {
+        ev.preventDefault();
+        bar.up();
+    }
+    else if (ev.keyCode == 40) {
+        ev.preventDefault();
+        bar.down();
+    }
+    else if (ev.keyCode == 87) {
+        ev.preventDefault();
+        bar2.up();
+    }
+    else if (ev.keyCode == 83) {
+        ev.preventDefault();
+        bar2.down();
+    }
+    
+});
 
 window.requestAnimationFrame(controller);
 
